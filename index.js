a46d7db46525bdb185dcbae245a915170e9ec595
Jeffrey Macatunggal <jeff>
2023-10-26 11:00:52 +0800

    update link value

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-export const applyCustomCode = (externalCodeSetup: any) => {
-	// call custom code api here
+import React from "react";
+
+export const applyCustomCode = (externalCodeSetup) => {
+  externalCodeSetup.activitiesScreenApi.setActivityToViewModelFilter((viewModel, activity, depend) => {
+    const hrefRegex = /href="([^"]+)"/;
+    const match = viewModel.content.match(hrefRegex);
+
+    // Extracted href value
+    const hrefValue = match ? match[1] : null;
+    return {
+      ...viewModel,
+      link: hrefValue,
+    };
+  });
 };
