363cbc9f5dd40592329f343cc25cb5455f1bc55e
Jeffrey Macatunggal <jeff>
2024-02-01 18:23:56 +0800

    added share button on topics

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import React from "react";
 import { NativeModules } from "react-native";
 import MessageSingleComponent from "./MessageSingleComponent";
 import ThreadItemHeader from "./ThreadItemHeader";
+import TopicTitle from "./TopicTitle";
+
 const { RNCustomCode } = NativeModules;
 
 export const applyCustomCode = (externalCodeSetup) => {
-  const { messagesScreenApi, messagesSingleScreenApi, activitiesScreenApi, navigationApi } = externalCodeSetup;
+  const { messagesScreenApi, messagesSingleScreenApi, activitiesScreenApi, topicSingleApi } = externalCodeSetup;
 
   messagesScreenApi.setMessageSingleComponent((props) => <MessageSingleComponent {...props} />);
   messagesSingleScreenApi.setThreadItemHeader((props) => <ThreadItemHeader {...props} />);
@@ -15,6 +17,8 @@ export const applyCustomCode = (externalCodeSetup) => {
 
     return [...buttonConfig];
   });
+
+  topicSingleApi.setTopicTitleComponent((props) => <TopicTitle data={props}></TopicTitle>);
 
   activitiesScreenApi.setActivityToViewModelFilter((viewModel, activity, depend) => {
     const hrefRegex = /href="([^"]+)"/;
