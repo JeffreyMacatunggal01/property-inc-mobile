5f95136cd86e1cc2aa4448e8befe0ce55a6ea40f
JeffreyMacatunggal01 <jeffreymacatunggal@gmail.com>
2024-03-12 11:44:23 +0800

    Update on native libraries

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { InAppBrowser } from 'react-native-inappbrowser-reborn'
+// import { InAppBrowser } from 'react-native-inappbrowser-reborn'
 import { NativeModules, Linking, View } from "react-native";
 import MessageSingleComponent from "./MessageSingleComponent";
 import ThreadItemHeader from "./ThreadItemHeader";
@@ -26,7 +26,7 @@ export const applyCustomCode = (externalCodeSetup) => {
               label: "Audio Call",
               isNavigation: true, //If set to true, the button will not be set to a "loading" state
               useDispatch: false, //If this is not set, `doFunction` will be wrapped in a `dispatch` function which is used to call a redux function
-              doFunction: async (a) => {
+              doFunction: (a) => {
 
                 // To Test - Maybe Try in Dev by using a Button add to maybe in a custom Screen?
                 var dataStr = JSON.stringify(a.id);
@@ -35,15 +35,15 @@ export const applyCustomCode = (externalCodeSetup) => {
                 var accUserLink = Object.values(dataDict['recipients'])[0]['user_link']; // https://property.inc/members/jeffrey18/bp-messages/ TOKEN /#/conversation/
                 var fullUrl = accUserLink + "bp-messages/#/conversation/" + convoID + "/?actions=bp-audio-call";
                 var callurl = "https://property.inc/?custom-link-jwt-generate=" + fullUrl;
-                await InAppBrowser.open(
-                  callurl,
-                  {
-                    // Include any other options as needed
-                    // ..
-                    // Enable microphone access
-                    mediaPlaybackRequiresUserAction: true,
-                  },
-                );
+                // await InAppBrowser.open(
+                //   callurl,
+                //   {
+                //     // Include any other options as needed
+                //     // ..
+                //     // Enable microphone access
+                //     mediaPlaybackRequiresUserAction: true,
+                //   },
+                // );
 
                 // a if stringify holds data in a json format
                 // data of interest is "id" for conversation identifier and
@@ -52,7 +52,8 @@ export const applyCustomCode = (externalCodeSetup) => {
                 // console.log(a, "dofunc");
                 // return Linking.openURL("https://property.inc/?custom-link-jwt-generate=https://property.inc/members?a=" + JSON.stringify(a));
                 // return Linking.openURL("https://property.inc/?custom-link-jwt-generate=" + fullUrl);
-                
+                // return Linking.openURL(callurl);
+                return Linking.openURL(fullUrl);
               },
             },
             {
@@ -60,26 +61,26 @@ export const applyCustomCode = (externalCodeSetup) => {
               label: "Video Call",
               isNavigation: true, //If set to true, the button will not be set to a "loading" state
               useDispatch: false, //If this is not set, `doFunction` will be wrapped in a `dispatch` function which is used to call a redux function
-              doFunction: async (a) => {
+              doFunction: (a) => {
                 var dataStr = JSON.stringify(a.id);
                 var dataDict = JSON.parse(dataStr);
                 var convoID = dataDict["id"];
                 var accUserLink = Object.values(dataDict['recipients'])[0]['user_link']; // https://property.inc/members/jeffrey18/bp-messages/ TOKEN /#/conversation/
                 var fullUrl = accUserLink + "bp-messages/#/conversation/" + convoID + "/?actions=bp-video-call";
                 var callurl = "https://property.inc/?custom-link-jwt-generate=" + fullUrl;
-                await InAppBrowser.open(
-                  callurl,
-                  {
-                    // Include any other options as needed
-                    // ..
-                    // Enable microphone access
-                    mediaPlaybackRequiresUserAction: true,
-                  },
-                );
+                // await InAppBrowser.open(
+                //   callurl,
+                //   {
+                //     // Include any other options as needed
+                //     // ..
+                //     // Enable microphone access
+                //     mediaPlaybackRequiresUserAction: true,
+                //   },
+                // );
 
                 // console.log(a, "dofunc");
                 // Linking.openURL("https://property.inc/?custom-link-jwt-generate=https://property.inc/members?a=" + JSON.stringify(a));
-                // return Linking.openURL("https://property.inc/?custom-link-jwt-generate=https://property.inc/members/jeffrey18/bp-messages/#/conversation/277/?actions=bp-audio-call");
+                return Linking.openURL(callurl);
               },
             },
           ],
@@ -92,6 +93,8 @@ export const applyCustomCode = (externalCodeSetup) => {
 
     return [...buttonConfig, newButton];
   });
+
+  
   externalCodeSetup.blogSingleApi.setBlogHeaderAvatar(BlogHeaderAvatar);
   topicSingleApi.setTopicTitleComponent(TopicTitle);
   activitiesScreenApi.setActivityToViewModelFilter((viewModel, activity, depend) => {
