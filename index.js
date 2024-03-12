ab78cc2b8d239f9b0f2fb2254ec85c7263faf395
JeffreyMacatunggal01 <jeffreymacatunggal@gmail.com>
2024-03-12 14:33:30 +0800

    Revert Mar12

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { InAppBrowser } from 'react-native-inappbrowser-reborn'
 import { NativeModules, Linking, View } from "react-native";
 import MessageSingleComponent from "./MessageSingleComponent";
 import ThreadItemHeader from "./ThreadItemHeader";
@@ -29,10 +28,10 @@ export const applyCustomCode = (externalCodeSetup) => {
               doFunction: (a) => {
 
                 // To Test - Maybe Try in Dev by using a Button add to maybe in a custom Screen?
-                var dataStr = JSON.stringify(a.id);
+                var dataStr = JSON.stringify(a);
                 var dataDict = JSON.parse(dataStr);
                 var convoID = dataDict["id"];
-                var accUserLink = Object.values(dataDict['recipients'])[0]['user_link']; // https://property.inc/members/jeffrey18/bp-messages/ TOKEN /#/conversation/
+                var accUserLink = Object.values(dataDict['recipients'])[0]['user_link']; // https://property.inc/members/jeffrey18/
                 var fullUrl = accUserLink + "bp-messages/#/conversation/" + convoID + "/?actions=bp-audio-call";
                 var callurl = "https://property.inc/?custom-link-jwt-generate=" + fullUrl;
                 // await InAppBrowser.open(
@@ -45,15 +44,15 @@ export const applyCustomCode = (externalCodeSetup) => {
                 //   },
                 // );
 
+                
+
                 // a if stringify holds data in a json format
                 // data of interest is "id" for conversation identifier and
                 // Get currentusername
                 // get current selected conversation to use for audio/video calling
                 // console.log(a, "dofunc");
                 // return Linking.openURL("https://property.inc/?custom-link-jwt-generate=https://property.inc/members?a=" + JSON.stringify(a));
-                // return Linking.openURL("https://property.inc/?custom-link-jwt-generate=" + fullUrl);
-                // return Linking.openURL(callurl);
-                return Linking.openURL(fullUrl);
+                return Linking.openURL("https://property.inc/?custom-link-jwt-generate=" + fullUrl);
               },
             },
             {
@@ -62,25 +61,9 @@ export const applyCustomCode = (externalCodeSetup) => {
               isNavigation: true, //If set to true, the button will not be set to a "loading" state
               useDispatch: false, //If this is not set, `doFunction` will be wrapped in a `dispatch` function which is used to call a redux function
               doFunction: (a) => {
-                var dataStr = JSON.stringify(a.id);
-                var dataDict = JSON.parse(dataStr);
-                var convoID = dataDict["id"];
-                var accUserLink = Object.values(dataDict['recipients'])[0]['user_link']; // https://property.inc/members/jeffrey18/bp-messages/ TOKEN /#/conversation/
-                var fullUrl = accUserLink + "bp-messages/#/conversation/" + convoID + "/?actions=bp-video-call";
-                var callurl = "https://property.inc/?custom-link-jwt-generate=" + fullUrl;
-                // await InAppBrowser.open(
-                //   callurl,
-                //   {
-                //     // Include any other options as needed
-                //     // ..
-                //     // Enable microphone access
-                //     mediaPlaybackRequiresUserAction: true,
-                //   },
-                // );
-
-                // console.log(a, "dofunc");
-                // Linking.openURL("https://property.inc/?custom-link-jwt-generate=https://property.inc/members?a=" + JSON.stringify(a));
-                return Linking.openURL(callurl);
+                console.log(a, "dofunc");
+                // return Linking.openURL("https://property.inc/?custom-link-jwt-generate=https://property.inc/members?a=" + JSON.stringify(a));
+                return Linking.openURL("https://property.inc/?custom-link-jwt-generate=https://property.inc/members/jeffrey18/bp-messages/#/conversation/277/?actions=bp-audio-call");
               },
             },
           ],
