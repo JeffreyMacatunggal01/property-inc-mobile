6137a1030ea2e6039e89332eb533902cd3368f95
vinzjaystone <vinzjaystone25@gmail.com>
2024-11-08 02:18:05 +0800

    Update dev branch simple/rebuild

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-export const applyCustomCode = (externalCodeSetup: any) => {
-	// call custom code api here
+import React from "react";
+import { NativeModules } from "react-native";
+const { RNCustomCode } = NativeModules;
+
+const MyCustomScreen = (props) => (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>My Custom screen</Text>
+    </View>
+);
+
+export const applyCustomCode = (externalCodeSetup) => {
+    const { navigationApi } = externalCodeSetup;
+
+    navigationApi.setFilterAfterAuthRoutes((afterAuthRoutes) => {
+        return {
+            ...afterAuthRoutes,
+            MyCustomScreen: MyCustomScreen,
+        };
+    });
+
+    navigationApi.setInitialAfterAuthRoute((props) => {
+        return "MyCustomScreen";
+    });
 };
